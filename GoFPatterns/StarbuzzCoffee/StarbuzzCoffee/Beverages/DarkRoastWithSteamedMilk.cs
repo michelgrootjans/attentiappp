0a8bbc6e26f6341231aaa6c6ceb98fe45ea415a4
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-14 08:43:36 +0000

    Added StarbuzzCoffee

---
+++ GoFPatterns/StarbuzzCoffee/StarbuzzCoffee/Beverages/DarkRoastWithSteamedMilk.cs
@@ -0,0 +1,15 @@
+﻿namespace StarbuzzCoffee.Beverages
+{
+    public class DarkRoastWithSteamedMilk : Beverage
+    {
+        public override string Description
+        {
+            get { return "Dark roast with steamed milk"; }
+        }
+
+        public override double Cost
+        {
+            get { return 6.5; }
+        }
+    }
+}
