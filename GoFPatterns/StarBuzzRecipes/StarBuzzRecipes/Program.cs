f18ae82c5f8644b5480703574b5d3a0f4c3decdc
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-14 12:30:25 +0000

    Added StarBuzzRecipes

---
+++ GoFPatterns/StarBuzzRecipes/StarBuzzRecipes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StarBuzzRecipes.Recipes;
+
+namespace StarBuzzRecipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffee = new Coffee();
+            coffee.Prepare();
+
+            var tea = new Tea();
+            tea.Prepare();
+
+            Console.ReadLine();
+        }
+    }
+}
