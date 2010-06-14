09be27458d28626a061d13b50fee4a43ddabba98
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-14 09:15:00 +0000

    Added PizzaStore

---
+++ GoFPatterns/PizzaStore/PizzaStore/Pizzas/GreekPizza.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaStore.Pizzas
+{
+    internal class GreekPizza : Pizza
+    {
+        public override string Name
+        {
+            get { return "Greek pizza"; }
+        }
+    }
+}
