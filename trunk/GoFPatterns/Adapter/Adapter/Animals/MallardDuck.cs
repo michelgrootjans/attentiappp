73a5c6437954f63f6d7509746b14ef58aa554753
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-14 11:38:50 +0000

    Added Adapter

---
+++ trunk/GoFPatterns/Adapter/Adapter/Animals/MallardDuck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adapter.Animals
+{
+    internal class MallardDuck : IDuck
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Mallard duck: I'm flying");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Mallard duck: Quack! (with a mallard accent)");
+        }
+    }
+}
