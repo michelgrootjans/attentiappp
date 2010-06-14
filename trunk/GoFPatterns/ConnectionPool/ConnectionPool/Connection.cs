a929d3ab3474f22cf797c9fca278aabcf0bf34dd
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-14 09:46:36 +0000

    Added ConnectionPool

---
+++ trunk/GoFPatterns/ConnectionPool/ConnectionPool/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConnectionPool
+{
+    internal class Connection
+    {
+        private readonly int connectionNumber;
+        private static int numberOfConnections;
+
+        public Connection()
+        {
+            connectionNumber = numberOfConnections++;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("Opening connection {0}", connectionNumber);
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Closing connection {0}", connectionNumber);
+        }
+    }
+}
---
+++ trunk/GoFPatterns/ConnectionPool/ConnectionPool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConnectionPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Connection connection = new Connection();
+                connection.Open();
+                //Do something useful here
+                connection.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
