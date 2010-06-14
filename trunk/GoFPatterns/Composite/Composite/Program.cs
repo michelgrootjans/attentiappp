17272aeb1b0dbb12ff05727479b1638c1a555544
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-14 13:18:16 +0000

    Added Composite

---
+++ trunk/GoFPatterns/Composite/Composite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Composite
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var menu = new FullMenu();
+
+            menu.PrintAllItems();
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class FullMenu
+    {
+        public void PrintAllItems()
+        {
+            Console.WriteLine("All menus");
+            Console.WriteLine("|- Pancake House Menu");
+            Console.WriteLine("|  | Pancake");
+            Console.WriteLine("|  | Maple sirop");
+            Console.WriteLine("|  | Sugar");
+            Console.WriteLine("|- Diner menu");
+            Console.WriteLine("|  | Fresh salad");
+            Console.WriteLine("|  | Jucy steak");
+            Console.WriteLine("|  | French fries");
+            Console.WriteLine("|  |- Desert menu");
+            Console.WriteLine("|  |  | Dame blache");
+            Console.WriteLine("|  |  | Apple pie");
+            Console.WriteLine("|- Cafe menu");
+            Console.WriteLine("|  | Home brew coffee");
+            Console.WriteLine("|  | Espresso");
+            Console.WriteLine("|  | Mint tea");
+        }
+    }
+}
