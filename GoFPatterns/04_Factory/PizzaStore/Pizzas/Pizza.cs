dd0f67cdb50df0a1bbf5932385fcf361435c9661
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-16 09:20:27 +0000

    Added sleeps for demo

--- GoFPatterns/04_Factory/PizzaStore/Pizzas/Pizza.cs
+++ GoFPatterns/04_Factory/PizzaStore/Pizzas/Pizza.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PizzaStore.Pizzas
 {
@@ -9,24 +10,31 @@ namespace PizzaStore.Pizzas
         public void Prepare()
         {
             Console.WriteLine("Preparing {0}", Name);
+            Thread.Sleep(500);
             Console.WriteLine("Tossing dough...");
+            Thread.Sleep(500);
             Console.WriteLine("Adding sauce...");
+            Thread.Sleep(500);
             Console.WriteLine("Adding toppings...");
+            Thread.Sleep(500);
         }
 
         public void Bake()
         {
             Console.WriteLine("Bake for 25 minutes at 250°C");
+            Thread.Sleep(2000);
         }
 
         public void Cut()
         {
             Console.WriteLine("Cutting the pizza into diagonal slices");
+            Thread.Sleep(500);
         }
 
         public void Box()
         {
             Console.WriteLine("Place pizza in official PizzaStore box");
+            Thread.Sleep(500);
         }
     }
 }
