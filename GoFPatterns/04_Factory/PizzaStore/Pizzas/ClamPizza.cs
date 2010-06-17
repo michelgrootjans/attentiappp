2ca3b331007b579a6245e4b74e99a0fbdc2765aa
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-17 06:30:35 +0000

    Added two Pizzas

---
+++ GoFPatterns/04_Factory/PizzaStore/Pizzas/ClamPizza.cs
@@ -0,0 +1,14 @@
+﻿namespace PizzaStore.Pizzas
+{
+    public class ClamPizza : Pizza
+    {
+        public ClamPizza()
+        {
+            Name = "Clam Pizza";
+            dough = "Thin Crust Dough";
+            sauce = "Marinara Souce";
+            toppings.Add("Reggiano");
+            toppings.Add("Fresh Clams");
+        }
+    }
+}
