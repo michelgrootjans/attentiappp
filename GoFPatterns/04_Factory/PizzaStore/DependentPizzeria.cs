de21d3bdbf1d2466f3dcb126babfa1d74e919b0a
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-17 07:54:51 +0000

    Added DependentPizzeria.cs

---
+++ GoFPatterns/04_Factory/PizzaStore/DependentPizzeria.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using PizzaStore.Pizzas;
+
+namespace PizzaStore
+{
+    internal class DependentPizzeria
+    {
+        public Pizza OrderPizza(string style, string type)
+        {
+            Pizza pizza = null;
+            if (style == "NY")
+            {
+                if (type == "cheese")
+                {
+                    pizza = new NYStyleCheesePizza();
+                }
+                else if (type == "clam")
+                {
+                    pizza = new NYStyleClamPizza();
+                }
+                else if (type == "veggie")
+                {
+                    pizza = new NYStyleVeggiePizza();
+                }
+                else if (type == "pepperoni")
+                {
+                    pizza = new NYStylePepperoniPizza();
+                }
+            }
+            else if (style == "Chicago")
+            {
+                if (type == "cheese")
+                {
+                    pizza = new ChicagoStyleCheesePizza();
+                }
+                else if (type == "clam")
+                {
+                    pizza = new ChicagoStyleClamPizza();
+                }
+                else if (type == "veggie")
+                {
+                    pizza = new ChicagoStyleVeggiePizza();
+                }
+                else if (type == "pepperoni")
+                {
+                    pizza = new ChicagoStylePepperoniPizza();
+                }
+            }
+            if (pizza == null)
+            {
+                throw new ArgumentException(string.Format("Unknown pizza: {0}.{1}", style, type));
+            }
+            return pizza;
+        }
+    }
+}
