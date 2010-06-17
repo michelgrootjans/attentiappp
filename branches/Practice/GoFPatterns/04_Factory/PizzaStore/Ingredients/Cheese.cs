b164f2e45ae802d2d683f5e64740f63c4739124f
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-17 09:29:00 +0000

    Merged from trunk

---
+++ branches/Practice/GoFPatterns/04_Factory/PizzaStore/Ingredients/Cheese.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PizzaStore.Ingredients
+{
+    internal abstract class Cheese
+    {
+        protected Cheese()
+        {
+            Console.WriteLine("Adding {0} ...", GetType().Name);
+        }
+    }
+
+    internal class MozarellaCheese : Cheese
+    {
+    }
+
+    internal class ReggianoCheese : Cheese
+    {
+    }
+}
