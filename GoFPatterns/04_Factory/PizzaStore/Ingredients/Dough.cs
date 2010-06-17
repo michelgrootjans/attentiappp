563e1a53ca3b6a05550a11100bd61120446fd59e
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-17 09:27:39 +0000

    Added prepared Ingredients

---
+++ GoFPatterns/04_Factory/PizzaStore/Ingredients/Dough.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PizzaStore.Ingredients
+{
+    internal class Dough
+    {
+        public Dough()
+        {
+            Console.WriteLine("Tossing {0} ...", GetType().Name);
+        }
+    }
+
+    internal class ThickCrustDough : Dough
+    {
+    }
+
+    internal class ThinCrustDough : Dough
+    {
+    }
+}
