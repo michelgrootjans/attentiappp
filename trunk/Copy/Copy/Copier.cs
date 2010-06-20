639a996431a03a660f395a68e1df061647216594
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-20 12:54:12 +0000

    Added the copy program

---
+++ trunk/Copy/Copy/Copier.cs
@@ -0,0 +1,14 @@
+﻿namespace Copy
+{
+    public class Copier
+    {
+        public static void Copy()
+        {
+            int c;
+            while ((c = Keyboard.Read()) != -1)
+            {
+                Printer.Write(c);
+            }
+        }
+    }
+}
