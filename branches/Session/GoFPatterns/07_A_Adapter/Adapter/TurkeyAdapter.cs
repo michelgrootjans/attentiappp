9795df45806f06bd93744ba17718a0327a72cc74
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-22 13:36:58 +0000

    Implemented TurkeyAdapter

---
+++ branches/Session/GoFPatterns/07_A_Adapter/Adapter/TurkeyAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Adapter.Animals;
+
+namespace Adapter
+{
+    internal class TurkeyAdapter : IDuck
+    {
+        private readonly ITurkey turkey;
+
+        public TurkeyAdapter(ITurkey turkey)
+        {
+            this.turkey = turkey;
+        }
+
+        public void Fly()
+        {
+            turkey.Fly();
+            turkey.Fly();
+            turkey.Fly();
+        }
+
+        public void Quack()
+        {
+            turkey.Gobble();
+        }
+    }
+}
