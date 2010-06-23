ed53d0ea4d659862422dc8fda5ca7085db4bc4b6
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-23 10:02:44 +0000

    Added an adapter for the CurrentDisplayAdapter

---
+++ GoFPatterns/02_Observer/WeatherMonitor/CurrentDisplayAdapter.cs
@@ -0,0 +1,19 @@
+﻿using WeatherMonitor.Displays;
+
+namespace WeatherMonitor
+{
+    public class CurrentDisplayAdapter
+    {
+        private readonly CurrentConditionsDisplay display;
+
+        public CurrentDisplayAdapter(CurrentConditionsDisplay display)
+        {
+            this.display = display;
+        }
+
+        public void UpdateDisplay(WeatherEventArgs e)
+        {
+            display.UpdateDisplay(e.Temperature, e.Humidity, e.Pressure);
+        }
+    }
+}
