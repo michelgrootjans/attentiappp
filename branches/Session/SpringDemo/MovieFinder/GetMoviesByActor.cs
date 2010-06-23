53947814dd959242f8ccdaea237cf92dabfd8406
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-23 18:31:02 +0000

    Implemented generic repository

---
+++ branches/Session/SpringDemo/MovieFinder/GetMoviesByActor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MovieFinder
+{
+    public class GetMoviesByActor : IQuery<IMovie>
+    {
+        private readonly string actor;
+
+        public GetMoviesByActor(string actor)
+        {
+            this.actor = actor;
+        }
+
+        public IEnumerable<IMovie> Execute(ISession session)
+        {
+            yield return new Movie("Dances with wolves");
+            yield return new Movie("Waterwolrd");
+        }
+    }
+}
---
+++ branches/Session/SpringDemo/MovieFinder/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MovieFinder
+{
+    public interface IRepository
+    {
+        IEnumerable<T> Query<T>(IQuery<T> queryObject);
+    }
+
+    public interface IQuery<T>
+    {
+        IEnumerable<T> Execute(ISession session);
+    }
+
+    public interface ISession
+    {
+    }
+}
