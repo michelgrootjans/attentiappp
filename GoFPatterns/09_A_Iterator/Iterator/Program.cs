a5ce2b756b69bb0bbcef2ea942cdbb3e68cfa0ca
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-23 14:59:38 +0000

    .net yield return implementation as Iterator/Program.cs

--- GoFPatterns/09_A_Iterator/Iterator/Program.cs
+++ GoFPatterns/09_A_Iterator/Iterator/Program.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Iterator
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            //what is an iterator?
+            var dateRange = new DateRange(DateTime.Now, DateTime.Now.AddDays(10));
+
+            var dateTimes = dateRange.Where(d => d.DayOfWeek != DayOfWeek.Sunday);
+
+            foreach (var date in dateTimes.ToList())
+            {
+                Console.WriteLine(date);
+            }
+
             Console.ReadLine();
+        }
+    }
+
+    internal class DateRange : IEnumerable<DateTime>
+    {
+        private readonly DateTime start;
+        private readonly DateTime end;
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public IEnumerator<DateTime> GetEnumerator()
+        {
+            var numberOfLoops = 0;
+            for (var day = start.Date; day <= end; day = day.AddDays(1))
+            {
+                if (numberOfLoops > 5)
+                    break;
+                yield return day;
+                numberOfLoops++;
+            }
+            yield return new DateTime(2010, 1, 1);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
