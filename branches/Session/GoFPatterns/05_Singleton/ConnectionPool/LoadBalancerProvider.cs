d1ab05314d5bb247d5d2fd94f214125b6bd5db83
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-28 08:33:46 +0000

    Implemented Singleton

---
+++ branches/Session/GoFPatterns/05_Singleton/ConnectionPool/LoadBalancerProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoadBalancer
+{
+    internal class LoadBalancerProvider
+    {
+
+        internal static SimpleLoadBalancer Instance
+        {
+            get { return SimpleLoadBalancer.Instance; }
+        }
+
+        internal class SimpleLoadBalancer
+        {
+            private readonly List<Server> servers;
+            private readonly Random random = new Random();
+            private static readonly SimpleLoadBalancer instance = new SimpleLoadBalancer();
+
+            private SimpleLoadBalancer()
+            {
+                servers = new List<Server>
+                          {
+                              new Server(),
+                              new Server()
+                          };
+            }
+
+            public static SimpleLoadBalancer Instance
+            {
+                get { return instance; }
+            }
+
+            internal Server GetAvailableServer()
+            {
+                return servers[random.Next(servers.Count)];
+            }
+        }
+    }
+}
