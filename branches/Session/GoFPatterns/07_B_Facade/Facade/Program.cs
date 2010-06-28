d14ca80e35716f6eec6113764190c5aa5c35540a
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-28 08:58:35 +0000

    Implemented Facade.csproj

--- branches/Session/GoFPatterns/07_B_Facade/Facade/Program.cs
+++ branches/Session/GoFPatterns/07_B_Facade/Facade/Program.cs
@@ -1,45 +1,19 @@
 ﻿using System;
+using System.Threading;
 using Facade.Appliances;
 
 namespace Facade
 {
     class Program
     {
-        private static readonly PopcornMaching popcornMachine = new PopcornMaching();
-        private static readonly Lights lights = new Lights();
-        private static readonly ProjectorScreen screen = new ProjectorScreen();
-        private static readonly LcdProjector projector = new LcdProjector();
-        private static readonly Amplifier amplifier = new Amplifier();
-        private static readonly HdDvdPlayer dvdPlayer = new HdDvdPlayer();
-
         static void Main()
         {
             var movie = new Dvd("Harry Potter 8");
+            var party = new PartyUtility(movie);
+            party.SwitchOn();
 
-            //start making popcorn
-            popcornMachine.SwitchOn();
-            popcornMachine.StartPopping();
-
-            //dim the lights
-            lights.Dim(10);
-
-            //lower the screen
-            screen.Down();
-
-            //turn projector on
-            projector.On();
-            projector.SetInput(dvdPlayer);
-            projector.SetWidescreenMode();
-
-            //switch the amp on
-            amplifier.On();
-            amplifier.SetVideoSource(dvdPlayer);
-            amplifier.SurroundSound = true;
-            amplifier.Volume = 5;
-
-            //play movie
-            dvdPlayer.TurnOn();
-            dvdPlayer.Play(movie);
+            Thread.Sleep(5000);
+            party.SwitchOff();
 
             Console.ReadLine();
         }
