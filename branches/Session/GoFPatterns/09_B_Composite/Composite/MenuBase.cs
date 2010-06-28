c76490abcc583c6c4c25e746cf09501819777a0d
michel.grootjans <michel.grootjans@dc8bae63-d195-f593-9663-cce4253fcc4e>
2010-06-28 08:08:26 +0000

    Implemented Composite.csproj

---
+++ branches/Session/GoFPatterns/09_B_Composite/Composite/MenuBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Composite
+{
+    internal abstract class MenuBase : IMenu
+    {
+        public void Print()
+        {
+            Print(0);
+        }
+
+        internal abstract void Print(int level);
+    }
+}
